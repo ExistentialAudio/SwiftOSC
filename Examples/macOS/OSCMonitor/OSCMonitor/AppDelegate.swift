acc2f3a674939750c908155007cbb970a3224f96
Devin Roth <devinroth@outlook.com>
2016-07-16 20:54:26 -0700

    Shut down server when application closes

--- Examples/macOS/OSCMonitor/OSCMonitor/AppDelegate.swift
+++ Examples/macOS/OSCMonitor/OSCMonitor/AppDelegate.swift
@@ -22,6 +22,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        server.stop()
     }
     
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
