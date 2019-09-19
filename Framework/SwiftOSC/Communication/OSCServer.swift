3b0bad0aa67791dcb9fbe6d3e4667a929afbb19b
Devin Roth <devin@devinrothmusic.com>
2019-09-18 22:31:16 -0700

    Better optional chaining for safety.

--- Framework/SwiftOSC/Communication/OSCServer.swift
+++ Framework/SwiftOSC/Communication/OSCServer.swift
@@ -125,59 +125,60 @@ public class OSCServer {
             
             //extract address and check if valid
             let addressEnd = messageData.firstIndex(of: 0x00)!
-            let addressString = messageData.subdata(in: 0..<addressEnd).toString()
-            var address = OSCAddressPattern()
-            if address.valid(addressString) {
-                address.string = addressString
-                message = OSCMessage(address)
+            if let addressString = messageData.subdata(in: 0..<addressEnd).toString() {
+                var address = OSCAddressPattern()
+                if address.valid(addressString) {
+                    address.string = addressString
+                    message = OSCMessage(address)
 
-                //extract types
-                messageData = messageData.subdata(in: (addressEnd/4+1)*4..<messageData.count)
-                let typeEnd = messageData.firstIndex(of: 0x00)!
-                let type = messageData.subdata(in: 1..<typeEnd).toString()
+                    //extract types
+                    messageData = messageData.subdata(in: (addressEnd/4+1)*4..<messageData.count)
+                    if let typeEnd = messageData.firstIndex(of: 0x00){
+                        if let type = messageData.subdata(in: 1..<typeEnd).toString() {
 
-                messageData = messageData.subdata(in: (typeEnd/4+1)*4..<messageData.count)
+                            messageData = messageData.subdata(in: (typeEnd/4+1)*4..<messageData.count)
 
-                for char in type {
-                    switch char {
-                    case "i"://int
-                        message?.add(Int(messageData.subdata(in: Range(0...3))))
-                        messageData = messageData.subdata(in: 4..<messageData.count)
-                    case "f"://float
-                        message?.add(Float(messageData.subdata(in: Range(0...3))))
-                        messageData = messageData.subdata(in: 4..<messageData.count)
-                    case "s"://string
-                        let stringEnd = messageData.firstIndex(of: 0x00)!
-                        message?.add(String(messageData.subdata(in: 0..<stringEnd)))
-                        messageData = messageData.subdata(in: (stringEnd/4+1)*4..<messageData.count)
-                    case "b": //blob
-                        var length = Int(messageData.subdata(in: Range(0...3)).toInt32())
-                        messageData = messageData.subdata(in: 4..<messageData.count)
-                        message?.add(Blob(messageData.subdata(in: 0..<length)))
-                        while length%4 != 0 {//remove null ending
-                            length += 1
-                        }
-                        messageData = messageData.subdata(in: length..<messageData.count)
+                            for char in type {
+                                switch char {
+                                case "i"://int
+                                    message?.add(Int(messageData.subdata(in: Range(0...3))))
+                                    messageData = messageData.subdata(in: 4..<messageData.count)
+                                case "f"://float
+                                    message?.add(Float(messageData.subdata(in: Range(0...3))))
+                                    messageData = messageData.subdata(in: 4..<messageData.count)
+                                case "s"://string
+                                    let stringEnd = messageData.firstIndex(of: 0x00)!
+                                    message?.add(String(messageData.subdata(in: 0..<stringEnd)))
+                                    messageData = messageData.subdata(in: (stringEnd/4+1)*4..<messageData.count)
+                                case "b": //blob
+                                    var length = Int(messageData.subdata(in: Range(0...3)).toInt32())
+                                    messageData = messageData.subdata(in: 4..<messageData.count)
+                                    message?.add(Blob(messageData.subdata(in: 0..<length)))
+                                    while length%4 != 0 {//remove null ending
+                                        length += 1
+                                    }
+                                    messageData = messageData.subdata(in: length..<messageData.count)
 
-                    case "T"://true
-                        message?.add(true)
-                    case "F"://false
-                        message?.add(false)
-                    case "N"://null
-                        message?.add()
-                    case "I"://impulse
-                        message?.add(Impulse())
-                    case "t"://timetag
-                        message?.add(Timetag(messageData.subdata(in: Range(0...7))))
-                        messageData = messageData.subdata(in: 8..<messageData.count)
-                    default:
-                        print("unknown osc type")
-                        message = nil
+                                case "T"://true
+                                    message?.add(true)
+                                case "F"://false
+                                    message?.add(false)
+                                case "N"://null
+                                    message?.add()
+                                case "I"://impulse
+                                    message?.add(Impulse())
+                                case "t"://timetag
+                                    message?.add(Timetag(messageData.subdata(in: Range(0...7))))
+                                    messageData = messageData.subdata(in: 8..<messageData.count)
+                                default:
+                                    print("unknown osc type")
+                                    message = nil
+                                }
+                            }
+                        } else {message = nil}
                     }
-                }
-            } else {
-                message = nil
-            }
+                } else {message = nil}
+            } else {message = nil}
         }
         return message
     }
