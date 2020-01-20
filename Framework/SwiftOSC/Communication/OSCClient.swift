1ee3711a7bfab71018f7f042246a272bd01acfe2
GW <gwsounddsg@gmail.com>
2020-01-20 18:16:29 -0500

    adds markdowns to OSCClient, its properties, and its methods.

--- Framework/SwiftOSC/Communication/OSCClient.swift
+++ Framework/SwiftOSC/Communication/OSCClient.swift
@@ -1,34 +1,69 @@
 import Foundation
 
+
+
+
+/**
+  *For sending OSC messages to another OSC device on the network, or to another application on the current machine.*
+ */
 @objc open class OSCClient: NSObject {
+    
+    /// Returns the current address, or sets the address messages will be sent to.
     open var address: String {
         didSet {
             _ = client.close()
             client = UDPClient(addr: address, port: port)
         }
     }
+    
+    /// Returns the current port, or sets the port messages will be sent to.
     open var port: Int {
         didSet {
             _ = client.close()
             client = UDPClient(addr: address, port: port)
         }
     }
+
     var client: UDPClient
+        
     
-    public init(address: String, port: Int){
+    /**
+     *Creates a new OSCClient*
+     
+    - Parameters:
+       - address: Could be an IP address, or just "localhost" for sending internally in your computer
+       - port: Must be a vaild port that's not being used.
+    */
+    public init(address: String, port: Int) {
         self.address = address
         self.port = port
         client = UDPClient(addr: address, port: port)
         client.enableBroadcast()
     }
-    open func send(_ element: OSCElement){
+
+    
+    /**
+     *Send a message to current address and port*
+     
+    - Parameters:
+       - element: An OSCElement is an abstract protocol inherited by OSCMessage and OSCBundle.
+     
+    - Important:
+        If the OSCClient is not connected to a valid address and port, this method will not throw an error. You are responsible for managing
+        a vaild connection.
+    */
+    open func send(_ element: OSCElement) {
         let data = element.data
+        
         if data.count > 9216 {
             print("OSCPacket is too large. Must be smaller than 9200 bytes")
         } else {
             _ = client.send(data:data)
         }
     }
+    
+    
+    /// Closes the port when deinitialized.
     deinit {
         _ = client.close()
     }
