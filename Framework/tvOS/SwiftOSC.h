788985ed6c3d1a48420acb93c209dc269640962e
Devin Roth <devin@devinrothmusic.com>
2020-01-19 22:31:44 -0800

    Add watchOS and tvOS support.

---
+++ Framework/tvOS/SwiftOSC.h
@@ -0,0 +1,19 @@
+//
+//  watchOS.h
+//  watchOS
+//
+//  Created by Devin Roth on 2020-01-19.
+//  Copyright © 2020 Devin Roth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for watchOS.
+FOUNDATION_EXPORT double tvOSVersionNumber;
+
+//! Project version string for watchOS.
+FOUNDATION_EXPORT const unsigned char tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <watchOS/PublicHeader.h>
+
+
